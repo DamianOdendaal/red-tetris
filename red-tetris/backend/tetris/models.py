285c1871b689a89cffd9c23867ad158b4a73cded
damianodendaal <dodendaa@student.wethinkcode.co.za>
2020-08-25 09:29:34 +0200

    creating the start app content for backend

--- red-tetris/backend/tetris/models.py
+++ red-tetris/backend/tetris/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class TetrisPlayer(models.Model):
+    game_name = models.CharField(max_length=120)
+    game_score = models.TextField()
+    #cater for the amount of times played
+
+    def __str__(self):
+        return self.game_name
+
+class TetrisGameModes(models.Model):
+    def __init__(self):
+        pass
+        # game modes come in here
+
