2e5a007939b62c1b392dbdf91076821ff4a8f130
damianodendaal <dodendaa@student.wethinkcode.co.za>
2020-10-04 22:38:54 +0200

    finished

--- red-tetris/backend/tetris/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import TetrisPlayer
-from .models import TetrisGameModes
-
-
-admin.site.register(TetrisPlayer)
-admin.site.register(TetrisGameModes)
--- red-tetris/frontend/docker_frontend.sh
+++
@@ -1,4 +0,0 @@
-docker build -t frontend:latest frontend
-docker run -v $PWD/frontend:/app frontend:latest npx create-react-app hello-world
-mv frontend/hello-world/* frontend/hello-world/.gitignore frontend/ && rmdir frontend/hello-world
-docker run -v $PWD/frontend:/app -p 3000:3000 frontend:latest npm start
